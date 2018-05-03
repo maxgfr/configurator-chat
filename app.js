db2fa1f77d938ca81ae96a42445e491744aa6469
Maxime Golfier <maxgolfier@gmail.com>
2018-05-03 18:01:11 +0200

    message

--- app.js
+++ app.js
@@ -9,6 +9,34 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var cfenv = require("cfenv");
+
+
+
+/***** IBM part *****/
+var vcapLocal; // load local VCAP configuration  and service credentials
+try {
+  vcapLocal = require('./vcap-local.json');
+  //console.log("Loaded local VCAP", vcapLocal);
+} catch (e) { }
+
+const appEnvOpts = vcapLocal ? { vcap: vcapLocal} : {}
+
+const appEnv = cfenv.getAppEnv(appEnvOpts);
+
+if (appEnv.services['conversation']) {
+  // Load the Watson Conversation library.
+  var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+  // Initialize database with credentials
+  conversation = new ConversationV1({
+    username: appEnv.services['conversation'][0].credentials.username,
+    password: appEnv.services['conversation'][0].credentials.password,
+    version_date: ConversationV1.VERSION_DATE_2017_05_26
+  });
+  //console.log(conversation);
+}
+/***** IBM part *****/
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -21,6 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,46 @@
 var express = require('express');
 var router = express.Router();
 
+var context_array = [];
+var number = 0;
+var context = null;
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/',function(req, res, next) {
+    context_array = [];
+    number = 0;
+    context = null;
+    if(!conversation) {
+        console.log("Conversation non initialisée");
+        res.render('error');
+    } else {
+        console.log("Conversation initialisée");
+        res.render('index', { conversation: conversation});
+    }
+});
+
+router.post('/',function(req, res, next) {
+    //console.log(req.body.input);
+    //console.log(context);
+    conversation.message({
+        input: { text: req.body.input},
+        context: context_array[context_array.length-1] ,
+        workspace_id: '635a4d6e-022d-4e16-88d6-4844c2cdcc99'
+    }, function(err, response) {
+        if (err) {
+           console.error(err);
+       } else {
+            console.log(response);
+            var rep = response.output.text;
+            /*SAVE CONTEXT*/
+            context = response.context;
+            context_array[number] = response.context;
+            //console.log(context_array);
+            number++;
+            /*SAVE CONTEXT*/
+            res.send(rep);
+        }
+    });
 });
 
 module.exports = router;
